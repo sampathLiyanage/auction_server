5350899e987d72e4fd58020a24da74f1fb3e517a
Sampath Liyanage <plbsam@gmail.com>
2021-04-10 10:40:42 +0530

    initial commit with migrations

---
+++ database/migrations/2021_04_10_041014_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->dateTime('auction_end_time')->nullable();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+        Schema::table('items', function($table) {
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ database/migrations/2021_04_10_042416_create_bids_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('amount');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('item_id')->unsigned();
+            $table->boolean('is_auto_bid');
+            $table->timestamps();
+        });
+        Schema::table('bids', function($table) {
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->foreign('item_id')
+                ->references('id')
+                ->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
